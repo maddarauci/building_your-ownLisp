8fe5ff1ae116849b16d1c095d06974f17c46b07e
maddarauci <dominicd165@gmail.com>
2022-01-20 03:55:17 +1100

    modified:   README.md 	modified:   hello_world/hello_world.c 	new file:   hello_world/prompt.c

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -11,9 +11,40 @@ int add_together(int a, int b) {
 
 	we can use struture to represent more complex data types, for example to represent a point 
 	in 2D space we could create a structure called 'point' that packs together two float
-	(decimal) values called x and y. to declare structures we can use the 'struct'
+	(decimal) values called x and y. to declare structures we can use the 'struct' keyword in
+	conjunction with the 'tyepdef' keyword.
+	our declaration would look like this.
 */
 
+typedef struct {
+	float x;
+	float y;
+} point 
+/*
+we should place this definition above any function that wish to use it. this type
+is no different to the built in types, and we can use it in all the same ways.
+to access an individual field we use a dot ., followed by the name of the filed, 
+such as x.
+*/
+
+/*
+point p;
+p.x = 0.1;
+p.y = 10.0;
+*/
+
+/*
+Pointers:
+a pointer is a variation on a normal type where the type name is suffixed with 
+an asterisk. for example we could declare a pointer to an integer by writing
+'int*'. 'char** argv' is a pointer type, its a pointer to pointer character.
+and its used as the input to the main function.
+
+pointers are used for a whole number of different things such as for strings or lists.
+
+*/
+
+float lenght = sqrt(p.x * p.x * p.y * p.y);
 int main(int argc, char** argv) {
 	puts("Hello, World!");
 	int r = add_together(10, 5);
---
+++ hello_world/prompt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
+/* declare a buffer for user input of size 2048 */
+static char input[2048];
+
+int main(int argc, char** argv) {
+	/* Print Version and Exit Information */
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+
+	// an inifinte loop
+	while(1) {
+		//fputs("Lispy> ", stdout);
+		// output the prompt and get input
+		char* input = readline("Lispy> ");
+
+		// add input to history
+		add_history(input);
+
+		// read a line of user input to maximum size 2048
+		fgets(input, 2048, stdin);
+
+		// echo input back to user 
+		printf("No you're a %s", input);
+
+		// free retrieved input 
+		free(input)
+	}
+
+	return 0;
+}
